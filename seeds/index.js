aef2f399a9a2584427f51b8511c8b702eb445f73
sajoh002 <sajoh002@gmail.com>
2024-03-07 21:33:41 -0500

    add image to campground model and add basic styling to index, show, new, and edit pages

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum et cum consectetur, doloribus unde ipsam animi minus aspernatur magnam explicabo asperiores distinctio ipsum eveniet modi iusto facere! Numquam, exercitationem quo!",
+      price,
     });
     await camp.save();
   }
