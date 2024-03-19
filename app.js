65581e9700f45ec3347ca6e10fe0dc95be90f5fe
sajoh002 <sajoh002@gmail.com>
2024-03-18 20:26:08 -0400

    add passport

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const passportLocal = require("passport-local");
 const ExpressError = require("./utils/ExpressError");
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
+const User = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -39,6 +42,12 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
