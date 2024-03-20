a7414a043e11ee5e8500976fbebbbbb9e72a852f
sajoh002 <sajoh002@gmail.com>
2024-03-19 22:22:45 -0400

    add automatic login when user registers

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,16 @@ router.get("/register", (req, res) => {
 
 router.post(
   "/register",
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { username, email, password } = req.body;
       const user = new User({ email, username });
-      const registereduser = await User.register(user, password);
-      req.flash("success", "Welcome to YelpCamp!");
-      res.redirect("/campgrounds");
+      const registeredUser = await User.register(user, password);
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to YelpCamp!");
+        res.redirect("/campgrounds");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("register");
