710fe3fcdbe01d7b2d089550fe881fbc8f41db52
sajoh002 <sajoh002@gmail.com>
2024-03-29 20:49:49 -0400

    add sanitize html

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(mongoSanitize);
+app.use(mongoSanitize());
 app.use(session(sessionConfig));
 app.use(flash());
 
