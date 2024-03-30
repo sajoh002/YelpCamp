83aafcb9f63bf1fb198dc28d1aa0719bdc1aaaf0
sajoh002 <sajoh002@gmail.com>
2024-03-29 20:18:16 -0400

    add mongo sanitize

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const passportLocal = require("passport-local");
+const mongoSanitize = require("express-mongo-sanitize");
 const ExpressError = require("./utils/ExpressError");
 const User = require("./models/user");
 
@@ -46,6 +47,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize);
 app.use(session(sessionConfig));
 app.use(flash());
 
